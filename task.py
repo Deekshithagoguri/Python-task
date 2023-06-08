82e34c2157f55a33b92b2d430e58bc66f588d3f7
Deekshithagoguri <85682420+Deekshithagoguri@users.noreply.github.com>
2023-06-08 16:31:43 +0530

    Add files via upload

---
+++ task.py
@@ -0,0 +1,33 @@
+import os
+import shutil
+import logging
+entries = os.listdir('E:/')
+logging.basicConfig(filename="task.log",
+                    format='%(asctime)s %(message)s',
+                    filemode='w')
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+#print(entries)
+for i in entries:
+    if i==entries[0]:
+        print(i)
+        continue
+    inp = "E:/"+i
+    if os.path.isfile(inp):
+        n, e = os.path.splitext(i)
+        e=e[1:]
+        print(e,i)
+        folder = "Result/"+e
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+            print(folder+" is created succesfully.")
+            logger.info(folder+" is created succesfully.")
+        dest=folder+"/"+i
+        src="E:/"+i
+        if shutil.copy(src,dest):
+            print("Sucess")
+            logger.info(i+" is added into "+folder+" succesfully.")
+        else:
+            print("Failed")
+            logger.info(i+" adding into "+folder+" is failed.")
